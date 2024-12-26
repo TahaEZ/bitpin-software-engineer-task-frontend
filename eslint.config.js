9ec04895ccdced4fb8c0d3afe7eaa7c02903e1eb
Taha Ebrahimzade <taha.ebrahimzadeh.miyab@gmail.com>
2024-12-26 14:51:06 +0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "no-await-in-loop": "error",
+      "no-constant-binary-expression": "error",
+      "no-duplicate-imports": "error",
+      "no-new-native-nonconstructor": "error",
+      "no-promise-executor-return": "error",
+      "no-self-compare": "error",
+      "no-template-curly-in-string": "error",
+      "no-unmodified-loop-condition": "error",
+      "no-unreachable-loop": "error",
+      "no-unused-private-class-members": "error",
+      "no-use-before-define": "error",
+      "require-atomic-updates": "error",
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      camelcase: "error",
+    },
+  }
+);
