d881f8742bb0bc25c08139ac1ec1fd1e312a79a9
Taha Ebrahimzade <taha.ebrahimzadeh.miyab@gmail.com>
2024-12-29 23:32:08 +0330

    Add repo address to base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bitpin-software-engineer-task-frontend',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
